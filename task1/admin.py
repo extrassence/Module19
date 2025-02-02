64294623e4db2e033aa2d628dcde954f48ae62c3
extrassence <extrassence@yandex.ru>
2025-02-02 22:43:39 +0300

    Added superuser and admin models

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('cost', 'size')
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_filter = ('balance', 'age')
+    list_per_page = 30
+    readonly_fields = ('balance',)
